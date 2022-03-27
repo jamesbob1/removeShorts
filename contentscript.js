6b4455f742328aa528f83eddef71465e4188dd73
Jamesbob <123.alon.alexander@gmail.com>
2022-03-27 16:54:44 +0300

    working version

--- contentscript.js
+++ contentscript.js
@@ -1,30 +1,33 @@
 /*jshint esversion: 6*/
-console.log("Shorts Remover");
 
-function newVideo(timeOverlay)
-{   
+
+function newVideo(timeOverlay) {
     let video = timeOverlay.parentNode.parentNode.parentNode.parentNode.parentNode;
     if (timeOverlay.getAttribute("overlay-style") == "SHORTS")
-         video.hidden = true;
+        video.hidden = true;
 }
 
 function sectionsObservered(mutations) {
     for (let mutation of mutations) {
         if (mutation.target.tagName == "YTD-THUMBNAIL-OVERLAY-TIME-STATUS-RENDERER")
-                newVideo(mutation.target);
-      }
+            newVideo(mutation.target);
+    }
 }
 
 function newSection(section) {
     let videosContainer = section.querySelector("#items");
     let videos = videosContainer.children;
-    
+
     let observer = new MutationObserver(sectionsObservered);
-    observer.observe(videosContainer, {subtree: true, attributes: true, attributeFilter: ["overlay-style"]});
-    
-    for(let video of videos) {
+    observer.observe(videosContainer, {
+        subtree: true,
+        attributes: true,
+        attributeFilter: ["overlay-style"]
+    });
+
+    for (let video of videos) {
         let el = video.querySelector("ytd-thumbnail-overlay-time-status-renderer");
-        if (el!= null)
+        if (el != null)
             newVideo(el);
         else
             break;
@@ -35,22 +38,44 @@ function sectionsContainerObservered(mutations) {
     for (let mutation of mutations) {
         if (mutation.type === "childList") {
             for (let node of mutation.addedNodes) {
-                if (node.nodeName==="YTD-ITEM-SECTION-RENDERER")
+                if (node.nodeName === "YTD-ITEM-SECTION-RENDERER")
                     newSection(node);
             }
         }
     }
 }
 
+function waitForSectionsContainerToLoad(page) {
+    let checkExist = setInterval(function () {
+        let sectionsContainer = document.querySelector("ytd-section-list-renderer > #contents");
+        if (sectionsContainer !== null) {
+            [...sectionsContainer.children].filter((node) => node.nodeName === "YTD-ITEM-SECTION-RENDERER").forEach(newSection);
 
-let checkExist = setInterval(function() {
-    let sectionsContainer = document.querySelector("ytd-section-list-renderer > #contents");
-    if (sectionsContainer !== null)
-    {   
-        [...sectionsContainer.children].filter((node)=>node.nodeName==="YTD-ITEM-SECTION-RENDERER").forEach(newSection);
+            let observer = new MutationObserver(sectionsContainerObservered);
+            observer.observe(sectionsContainer, {
+                childList: true
+            });
+            clearInterval(checkExist);
+        }
+    }, 100);
+}
 
-        let observer = new MutationObserver(sectionsContainerObservered);
-        observer.observe(sectionsContainer, {childList: true});
-        clearInterval(checkExist);
+function pageManagerUpdate(mutations) {
+    pages = pageManager.children;
+
+    for (let page of pageManager.children) {
+        if (page.tagName == "YTD-BROWSE" && page.baseURI == "https://www.youtube.com/feed/subscriptions") {
+            console.log("SHORTS remover NOW!!!!");
+            pageManagerObserver.disconnect();
+            waitForSectionsContainerToLoad(page);
+            break;
+        }
     }
-}, 100);
+}
+
+
+let pageManager = document.getElementById("page-manager");
+let pageManagerObserver = new MutationObserver(pageManagerUpdate);
+pageManagerObserver.observe(pageManager, {
+    childList: true
+});
